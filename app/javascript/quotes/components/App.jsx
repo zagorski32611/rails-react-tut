74950f39c1f207e853da358428093749abbea3d0
Joe Zagorski <j.j.zagorski@gmail.com>
2018-04-05 21:23:12 -0400

    it works! time to add style

--- app/javascript/quotes/components/App.jsx
+++ app/javascript/quotes/components/App.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import {
-    BrowserRouter as Router,
-    Route
+  BrowserRouter as Router,
+  Route
 } from 'react-router-dom'
 import QuotesDisplay from './QuotesDisplay'
 
 const App = (props) => (
-    <Router>
-        <div>
-            <Route
-                path = '/'
-                component={QuotesDisplay}
-            />
-        </div>
-    </Router>
+  <Router startingQuoteId={props.startingQuoteId}>
+		<div>
+			<Route
+					path='/'
+					startingQuoteId={props.startingQuoteId}
+					render={(routeProps) => <QuotesDisplay {...props}
+					{...routeProps} />}
+			/>
+		</div>
+  </Router>
 )
+// You will need this on the bottom of each component file
+// to make it available through ES6 'import' statements.
 
 export default App
--- app/javascript/quotes/index.js
+++ app/javascript/quotes/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import ReactDom from 'react-dom'
+import ReactDOM from 'react-dom'
 import App from './components/App'
 
+
 const quotes = document.querySelector('#quotes')
-ReactDom.render(<App />, quotes)
+ReactDOM.render(<App startingQuoteId={quotes.CDATA_SECTION_NODE.startingQuoteId}/>, quotes)
